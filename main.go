10937927102fb13811726e10d5ecfa3e23c0aa8f
moqsien <moqsien@foxmail.com>
2023-08-30 21:51:32 +0800

    trojan

--- main.go
+++ main.go
@@ -1,14 +1,15 @@
 package main
 
 import (
-	"github.com/moqsien/vpnparser/pkgs/outbound/xray"
-	_ "github.com/moqsien/vpnparser/pkgs/parser"
+	_ "github.com/moqsien/vpnparser/pkgs/outbound/xray"
+	"github.com/moqsien/vpnparser/pkgs/parser"
 )
 
 func main() {
 	// parser.VlessTest()
+	parser.TrojanTest()
 	// s := xray.GetPattern()
 	// fmt.Println(s)
 	// xray.TestVmess()
-	xray.TestVless()
+	// xray.TestVless()
 }
--- pkgs/parser/trojan.go
+++ pkgs/parser/trojan.go
@@ -1,5 +1,13 @@
 package parser
 
+import (
+	"encoding/json"
+	"fmt"
+	"net/url"
+	"os"
+	"strconv"
+)
+
 /*
 trojan: ['allowInsecure', 'peer', 'sni', 'type', 'path', 'security', 'headerType']
 */
@@ -9,11 +17,62 @@ type ParserTrojan struct {
 	Port     int
 	Password string
 
-	AllowInsecure int
+	AllowInsecure string
 	HeaderType    string
 	Path          string
 	Peer          string
 	Security      string
 	SNI           string
 	Type          string
+}
+
+func (that *ParserTrojan) Parse(rawUri string) {
+	if u, err := url.Parse(rawUri); err == nil {
+		that.Address = u.Hostname()
+		that.Port, _ = strconv.Atoi(u.Port())
+		that.Password = u.User.Username()
+
+		query := u.Query()
+		that.AllowInsecure = query.Get("allowInsecure")
+		that.HeaderType = query.Get("headerType")
+		that.Path = query.Get("path")
+		that.Peer = query.Get("peer")
+		that.Security = query.Get("security")
+		that.SNI = query.Get("sni")
+		that.Type = query.Get("type")
+	}
+}
+
+func (that *ParserTrojan) GetAddr() string {
+	return that.Address
+}
+
+func (that *ParserTrojan) GetPort() int {
+	return that.Port
+}
+
+func (that *ParserTrojan) Show() {
+	fmt.Printf("addr: %s, port: %v, password: %s", that.Address, that.Port, that.Password)
+}
+
+func TrojanTest() {
+	type Trojan struct {
+		Trojan []string `json:"Trojan"`
+	}
+
+	t := &Trojan{}
+	content, _ := os.ReadFile(`C:\Users\moqsien\data\projects\go\src\vpnparser\misc\trojan.json`)
+	json.Unmarshal(content, t)
+	i := 0
+	for _, rawUri := range t.Trojan {
+		p := &ParserTrojan{}
+		p.Parse(rawUri)
+		if p.Address != "" {
+			i++
+		}
+		if p.Security != "" {
+			fmt.Println(p.Type, p.Security, p.Path, p.SNI, p.HeaderType, p.AllowInsecure)
+		}
+	}
+	fmt.Println("total: ", i, len(t.Trojan))
 }
