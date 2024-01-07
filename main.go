163a3e8fb22ba3124e5705db602d272888a5a03c
moqsien <moqsien@foxmail.com>
2024-01-07 12:49:55 +0800

    update

--- main.go
+++ main.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"fmt"
-
-	"github.com/moqsien/vpnparser/pkgs/outbound"
+	"github.com/moqsien/vpnparser/pkgs/cmd"
 	_ "github.com/moqsien/vpnparser/pkgs/outbound/sing"
 	_ "github.com/moqsien/vpnparser/pkgs/outbound/xray"
 	_ "github.com/moqsien/vpnparser/pkgs/parser"
@@ -26,9 +24,9 @@ func main() {
 	// sing.TestTrojan()
 	// sing.TestSS()
 
-	// cmd.StartApp()
+	cmd.StartApp()
 
-	rawUri := "vmess://{\"add\":\"ms.shabijichang.com\",\"port\":\"80\",\"id\":\"f1865e50-2510-46d1-bcb2-e00b4b656305\",\"aid\":\"0\",\"scy\":\"auto\",\"net\":\"ws\",\"v\":\"2\",\"ps\":\"未知_0915019\",\"host\":\"\",\"path\":\"\",\"tls\":\"\",\"sni\":\"\",\"type\":\"none\",\"serverPort\":0,\"nation\":\"🏁ZZ\"}"
-	p := outbound.ParseRawUriToProxyItem(rawUri, outbound.SingBox)
-	fmt.Println(p.GetOutbound())
+	// rawUri := "vmess://{\"add\":\"ms.shabijichang.com\",\"port\":\"80\",\"id\":\"f1865e50-2510-46d1-bcb2-e00b4b656305\",\"aid\":\"0\",\"scy\":\"auto\",\"net\":\"ws\",\"v\":\"2\",\"ps\":\"未知_0915019\",\"host\":\"\",\"path\":\"\",\"tls\":\"\",\"sni\":\"\",\"type\":\"none\",\"serverPort\":0,\"nation\":\"🏁ZZ\"}"
+	// p := outbound.ParseRawUriToProxyItem(rawUri, outbound.SingBox)
+	// fmt.Println(p.GetOutbound())
 }
